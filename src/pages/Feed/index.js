6ca5581ded9a2eecd00424b4fb9ba1c7f8718956
Wilson Neto Dev <contato@wilsonneto.com.br>
2019-11-19 13:45:02 -0300

    fix the rendering page

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
 
-import 'style.scss';
+import './style.scss';
+
+/*
+import mode from '../../assets/';
+import like from '../../assets/';
+import comment from '../../assets/';
+import send from '../../assets/';
+*/
 
 class Feed extends Component {
   render() {
-    return <div>Feed</div>;
+    return (
+      <sectionpost-list>
+        <article>
+          <header>
+            <div className="user-info">
+              <span>Wilson Neto</span>
+              <span className="place">Rio do Sul</span>
+            </div>
+          </header>
+        </article>
+      </sectionpost-list>
+    );
   }
 }
 
